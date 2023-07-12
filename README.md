74770d2e1ce942d0ab00fb92d9514049169088f7
bharatdogra <bharatmdogra@gmail.com>
2023-07-12 19:51:41 +0530

    yaml stage 3 added and made changes in dvc yaml

--- README.md
+++ README.md
@@ -24,5 +24,6 @@ dvc repro
 
 ```
 dvc dag
+## direct acyclic graph
 ```
 
--- stage_02.py
+++ stage_02.py
@@ -3,3 +3,5 @@ with open('artifact.txt','r') as f:
     text = f.read()
 
 print(text)
+print('this is the end of stage 2')
+print('Adding stage 03')
